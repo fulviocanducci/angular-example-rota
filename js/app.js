3171c89530ca4134fe846c20ced8b34d29f3d902
fulviocanducci <fulviocanducci@hotmail.com>
2017-07-15 22:22:06 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var app = angular.module('app', ['ngRoute']);
+
+    app.config(function($routeProvider, $locationProvider){        
+        $routeProvider
+        .when('/', {
+            templateUrl: 'template/index.html',
+            controller: 'indexCtrl'
+        })
+        .when('/create', {
+            templateUrl: 'template/create.html',
+            controller: 'createCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });        
+    });
+
+    app.controller("indexCtrl", ["$scope", "$location", function($scope, $location)
+    {
+        /*$scope.gocreate = function()
+        {
+             $location.path('/create');
+        }*/
+
+    }]);
+
+    app.controller("createCtrl", ["$scope", "$location", function($scope, $location)
+    {        
+        $scope.nome = "Fúlvio";
+    }]);
